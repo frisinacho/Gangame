5c46ddd3e68867557f9ec23fd7d18b7f6e9da7be
ignacio.lasaosa <frisinacho@gmail.com>
2018-04-26 14:08:53 +0200

    navigationView.setOnNavigationItemSelectedListener

--- app/src/main/java/com/frisinacho/gangame/MainActivity.kt
+++ app/src/main/java/com/frisinacho/gangame/MainActivity.kt
@@ -2,6 +2,8 @@ package com.frisinacho.gangame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigationView.selectedItemId = DEFAULT_OPTION
+        navigationView.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
