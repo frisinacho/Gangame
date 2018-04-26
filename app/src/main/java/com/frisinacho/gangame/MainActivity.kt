51b3af49fcece1e5cda20b5efd2e42ee61f14d80
ignacio.lasaosa <frisinacho@gmail.com>
2018-04-26 14:10:57 +0200

    Convert function to Lambda expression.

--- app/src/main/java/com/frisinacho/gangame/MainActivity.kt
+++ app/src/main/java/com/frisinacho/gangame/MainActivity.kt
@@ -2,8 +2,6 @@ package com.frisinacho.gangame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigationView.selectedItemId = DEFAULT_OPTION
-        navigationView.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-        })
+        navigationView.setOnNavigationItemSelectedListener { item ->
+            true
+        }
     }
 }
