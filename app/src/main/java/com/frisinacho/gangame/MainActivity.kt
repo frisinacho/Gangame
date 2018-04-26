9c0db38467497c7748e06d4bfb3818d8cc6c32fd
ignacio.lasaosa <frisinacho@gmail.com>
2018-04-26 13:52:38 +0200

    Using Kotlin-Android-Extensions.

--- app/src/main/java/com/frisinacho/gangame/MainActivity.kt
+++ app/src/main/java/com/frisinacho/gangame/MainActivity.kt
@@ -2,11 +2,14 @@ package com.frisinacho.gangame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationView.selectedItemId = R.id.action_deals
     }
 }
