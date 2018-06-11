e2de35ce98b22bab0874b9423560de0520aaeffe
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-11 16:52:28 +0200

    Models file for Deal and TopGame

---
+++ app/src/main/java/com/frisinacho/gangame/Models.kt
@@ -0,0 +1,16 @@
+package com.frisinacho.gangame
+
+data class Deal(var title: String,
+                var salePrice: Float,
+                var normalPrice: Float,
+                var metacriticScore: Int,
+                var steamRating: Int,
+                var thumb: String)
+
+data class TopGame(var title: String,
+                   var owners: Int,
+                   var steamRating: Int,
+                   var publisher: String,
+                   var price: Float,
+                   var position: Int,
+                   var thumb: String)
