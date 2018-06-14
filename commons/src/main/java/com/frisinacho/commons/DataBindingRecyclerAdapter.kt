35bd3ae108db87028909d77e2d788ade1827d05d
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-14 13:43:17 +0200

    inflate onCreateViewHolder.

--- commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
+++ commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
@@ -3,14 +3,17 @@ package com.frisinacho.commons
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 
-class DataBindingRecyclerAdapter<MODEL> : RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
+class DataBindingRecyclerAdapter<MODEL> (val itemLayoutResId: Int) : RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
+
+    val items = mutableListOf<MODEL>()
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = parent.inflate(itemLayoutResId)
+        return DataBindingViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: DataBindingViewHolder<MODEL>, position: Int) {
