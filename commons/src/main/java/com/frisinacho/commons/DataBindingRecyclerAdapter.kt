9c96a45e05cd908def92db629d4b53955ea0f0ec
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-14 13:38:06 +0200

    Create DataBinding ViewHolder & RecyclerAdapter.

---
+++ commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
@@ -0,0 +1,19 @@
+package com.frisinacho.commons
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class DataBindingRecyclerAdapter<MODEL> : RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<MODEL>, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ commons/src/main/java/com/frisinacho/commons/DataBindingViewHolder.kt
@@ -0,0 +1,11 @@
+package com.frisinacho.commons
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class DataBindingViewHolder<MODEL>(view: View) : RecyclerView.ViewHolder(view){
+
+    fun bindItem(item: MODEL) {
+
+    }
+}
