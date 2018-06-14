8a4ca6d32245e3acc72651c38e335aac0b1d5d4b
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-14 13:39:42 +0200

    Reorder list.

--- commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
+++ commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
@@ -4,11 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 
 class DataBindingRecyclerAdapter<MODEL> : RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
+
+    override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getItemCount(): Int {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
