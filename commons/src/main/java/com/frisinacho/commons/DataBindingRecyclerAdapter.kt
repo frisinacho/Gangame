2e9655f923d75bf325638e423f60a2e300c71429
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-14 13:47:56 +0200

    binding ViewHolder.

--- commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
+++ commons/src/main/java/com/frisinacho/commons/DataBindingRecyclerAdapter.kt
@@ -1,6 +1,9 @@
 package com.frisinacho.commons
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
 class DataBindingRecyclerAdapter<MODEL> (val itemLayoutResId: Int) : RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
@@ -12,6 +15,11 @@ class DataBindingRecyclerAdapter<MODEL> (val itemLayoutResId: Int) : RecyclerVie
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
+        val binding : ViewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+                itemLayoutResId,
+                parent,
+                false)
+
         val view = parent.inflate(itemLayoutResId)
         return DataBindingViewHolder(view)
     }
