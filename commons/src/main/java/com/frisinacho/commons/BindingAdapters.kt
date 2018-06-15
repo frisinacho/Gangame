9b6a1810855e33a66c14fc115ff4aa10f36faa77
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-15 12:55:02 +0200

    Binding Adapters.

---
+++ commons/src/main/java/com/frisinacho/commons/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.frisinacho.commons
+
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun loadImage(imageView: ImageView, url: String) {
+    Glide.with(imageView)
+            .load(url)
+            .into(imageView)
+}
+
