038d04b1163a216114cb0c617e276522dec9b970
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-15 13:11:08 +0200

    TopOwnedFragment.

--- app/src/main/java/com/frisinacho/gangame/owned/TopOwnedFragment.kt
+++ app/src/main/java/com/frisinacho/gangame/owned/TopOwnedFragment.kt
@@ -1,7 +1,38 @@
 package com.frisinacho.gangame.owned
 
-import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.frisinacho.commons.BR
+import com.frisinacho.commons.BaseListFragment
+import com.frisinacho.commons.DataBindingRecyclerAdapter
+import com.frisinacho.gangame.R
+import com.frisinacho.gangame.TopGame
 
-class TopOwnedFragment : Fragment(){
+class TopOwnedFragment : BaseListFragment(){
+    override fun getAdapter(): RecyclerView.Adapter<*> {
+        return DataBindingRecyclerAdapter<TopGame>(BR.topGame, R.layout.item_top_game)
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_top_owned
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (listAdapter as DataBindingRecyclerAdapter<TopGame>)
+                .items.addAll(getDummyTopGames())
+    }
+
+    fun getDummyTopGames(): ArrayList<TopGame> {
+        return arrayListOf(TopGame("Counter Strike",
+                13407338,
+                80,
+                "Valve",
+                9.99F,
+                1,
+                "http://cdn.akamai.steamstatic.com/steam/apps/10/capsule_184x69.jpg"))
+    }
 
 }
