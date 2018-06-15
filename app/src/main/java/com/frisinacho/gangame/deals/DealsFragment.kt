05556110329f273b1613d634f5772287b363d5ae
ignacio.lasaosa <frisinacho@gmail.com>
2018-06-15 12:57:50 +0200

    DealsFragment BaseListFragment.

--- app/src/main/java/com/frisinacho/gangame/deals/DealsFragment.kt
+++ app/src/main/java/com/frisinacho/gangame/deals/DealsFragment.kt
@@ -1,11 +1,16 @@
 package com.frisinacho.gangame.deals
 
+import android.support.v7.widget.RecyclerView
+import com.frisinacho.commons.BR
 import com.frisinacho.commons.BaseFragment
+import com.frisinacho.commons.BaseListFragment
+import com.frisinacho.commons.DataBindingRecyclerAdapter
+import com.frisinacho.gangame.Deal
 import com.frisinacho.gangame.R
 
-class DealsFragment : BaseFragment(){
-
-    override fun getLayoutResId(): Int {
-        return R.layout.fragment_deals
+class DealsFragment : BaseListFragment(){
+    override fun getAdapter(): RecyclerView.Adapter<*> {
+        return DataBindingRecyclerAdapter<Deal>(BR.deal,
+                R.layout.item_deal)
     }
 }
