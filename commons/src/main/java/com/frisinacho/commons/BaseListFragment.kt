484249132c022758e90494a6886fbad13abf8101
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-17 11:40:23 +0200

    lateinit to prevent errors.

--- commons/src/main/java/com/frisinacho/commons/BaseListFragment.kt
+++ commons/src/main/java/com/frisinacho/commons/BaseListFragment.kt
@@ -8,8 +8,7 @@ import kotlinx.android.synthetic.main.fragment_list.view.*
 
 abstract class BaseListFragment : BaseFragment() {
 
-    val listAdapter : RecyclerView.Adapter<*>
-        get() = getAdapter()
+    lateinit var listAdapter : RecyclerView.Adapter<*>
 
     override fun getLayoutResId(): Int {
         return R.layout.fragment_list
@@ -17,6 +16,7 @@ abstract class BaseListFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        listAdapter = getAdapter()
 
         view?.list?.let {
             with(view.list) {
