de63685cf1c7aff26d71e0160dc0594c2b9ee284
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-17 12:54:28 +0200

    Retrofit API init.

---
+++ gangame.sdk/src/main/java/com/frisinacho/gangamesdk/Routes.kt
@@ -0,0 +1,6 @@
+package com.frisinacho.gangamesdk
+
+object Routes {
+    const val BASE_URL_STEAM_SPY = "https://steamspy.com/api.php"
+    const val BASE_URL_CHEAP_SHARK = "https://cheapshark.com/api/1.0"
+}
