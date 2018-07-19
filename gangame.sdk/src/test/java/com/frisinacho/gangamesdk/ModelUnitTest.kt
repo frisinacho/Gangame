9ebdd3155c3654ee93a281db4a9bff2caafce152
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-19 13:14:33 +0200

    Asserts for topGameParsingTest

--- gangame.sdk/src/test/java/com/frisinacho/gangamesdk/ModelUnitTest.kt
+++ gangame.sdk/src/test/java/com/frisinacho/gangamesdk/ModelUnitTest.kt
@@ -27,6 +27,47 @@ class ModelUnitTest {
             "\"thumb\": \"http://cdn.akamai.steamstatic.com/steam/apps/38700/capsule_sm_120.jpg?t=1488471030\"" +
             "}"
 
+    val JSON_TOP_GAME = "{" +
+            "\"appid\": 10," +
+            "\"name\": \"Counter-Strike\"," +
+            "\"developer\": \"Valve\"," +
+            "\"publisher\": \"Valve\"," +
+            "\"score_rank\": 97," +
+            "\"owners\": 13364200," +
+            "\"owners_variance\": 105566," +
+            "\"players_forever\": 9404662," +
+            "\"players_forever_variance\": 88993," +
+            "\"players_2weeks\": 393588," +
+            "\"players_2weeks_variance\": 18406," +
+            "\"average_forever\": 11259," +
+            "\"average_2weeks\": 698," +
+            "\"median_forever\": 429," +
+            "\"median_2weeks\": 66," +
+            "\"ccu\": 18288," +
+            "\"price\": \"999\"," +
+            "\"tags\": {" +
+            "\"Action\": 2510," +
+            "\"FPS\": 1887," +
+            "\"Multiplayer\": 1524," +
+            "\"Shooter\": 1301," +
+            "\"Classic\": 1248," +
+            "\"Team-Based\": 849," +
+            "\"Competitive\": 719," +
+            "\"First-Person\": 713," +
+            "\"Tactical\": 664," +
+            "\"1990's\": 513," +
+            "\"e-sports\": 487," +
+            "\"PvP\": 402," +
+            "\"Military\": 309," +
+            "\"Strategy\": 287," +
+            "\"Score Attack\": 171," +
+            "\"Survival\": 156," +
+            "\"Assassin\": 133," +
+            "\"1980s\": 123," +
+            "\"Ninja\": 106," +
+            "\"Tower Defense\": 71" +
+            "}}"
+
     @Test
     fun dealParsingTest() {
         val gson = Gson()
@@ -42,6 +83,12 @@ class ModelUnitTest {
 
     @Test
     fun topGameParsingTest() {
+        val gson = Gson()
+        val topGame = gson.fromJson(JSON_TOP_GAME, TopGame::class.java)
 
+        Assert.assertEquals(topGame.title, "Counter-Strike")
+        Assert.assertEquals(topGame.steamRating, 97)
+        Assert.assertEquals(topGame.owners, 13364200)
+        Assert.assertEquals(topGame.publisher, "Valve")
     }
 }
