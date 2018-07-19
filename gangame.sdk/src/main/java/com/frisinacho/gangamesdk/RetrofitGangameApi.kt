fe31b91359e2f9f578df770819d36ca808aa4bf5
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-19 12:57:45 +0200

    All methods in RetrofitGangameApi.

--- gangame.sdk/src/main/java/com/frisinacho/gangamesdk/RetrofitGangameApi.kt
+++ gangame.sdk/src/main/java/com/frisinacho/gangamesdk/RetrofitGangameApi.kt
@@ -7,4 +7,10 @@ interface RetrofitGangameApi {
 
     @GET(Routes.GET_DEALS)
     fun getDeals(): Call<ArrayList<Deal>>
+
+    @GET(Routes.GET_TOP_100_GAMES)
+    fun getTopRatedGames(): Call<ArrayList<TopGame>>
+
+    @GET(Routes.GET_MOST_OWNED_GAMES)
+    fun getMostOwnedGames(): Call<ArrayList<TopGame>>
 }
