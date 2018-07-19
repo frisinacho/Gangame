56a0bb385ebe8e5af01f34ce5fde09bf77161050
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-19 16:31:55 +0200

    getResponseFor creation

--- gangame.sdk/src/main/mock/java/com.frisinacho.gangamesdk/MockResponseInterceptor.kt
+++ gangame.sdk/src/main/mock/java/com.frisinacho.gangamesdk/MockResponseInterceptor.kt
@@ -5,5 +5,10 @@ import okhttp3.Interceptor
 class MockResponseInterceptor: Interceptor{
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
+        val response = getResponseFor(request.url().toString())
+    }
+
+    private fun getResponseFor(ursl: String): String {
+
     }
 }
