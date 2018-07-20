83fa83f1e85399d5fe8ef0c08d4b8cdd5056b9bb
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-20 13:10:23 +0200

    TopRated and MostOwned Observables arrayLists.

--- app/src/main/java/com/frisinacho/gangame/data/GangameDataSource.kt
+++ app/src/main/java/com/frisinacho/gangame/data/GangameDataSource.kt
@@ -2,6 +2,7 @@ package com.frisinacho.gangame.data
 
 import io.reactivex.Observable
 import com.frisinacho.gangame.Deal
+import com.frisinacho.gangame.TopGame
 import com.frisinacho.gangamesdk.GangameApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -16,6 +17,36 @@ object GangameDataSource {
                     val deals = listDeal.map { deal -> DealMapper.fromSdk(deal) }
                     val arrayList = arrayListOf<Deal>()
                     arrayList.addAll(deals)
+                    arrayList
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+    }
+
+    fun getTopRated(): Observable<ArrayList<TopGame>>{
+        return apiService.apiClient
+                .getTopRatedGamesObservable()
+                .map { listGames ->
+                    val games = listGames.mapIndexed { index, game ->
+                        TopGameMapper.fromSdk(game, (index +1))
+                    }
+                    val arrayList = arrayListOf<TopGame>()
+                    arrayList.addAll(games)
+                    arrayList
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+    }
+
+    fun getMostOwned(): Observable<ArrayList<TopGame>>{
+        return apiService.apiClient
+                .getMostOwnedGamesObservable()
+                .map { listGames ->
+                    val games = listGames.mapIndexed { index, game ->
+                        TopGameMapper.fromSdk(game, (index +1))
+                    }
+                    val arrayList = arrayListOf<TopGame>()
+                    arrayList.addAll(games)
                     arrayList
                 }
                 .observeOn(AndroidSchedulers.mainThread())
