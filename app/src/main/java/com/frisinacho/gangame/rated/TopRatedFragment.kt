8d2f30fda30ac91fd1489774a14986d463d46918
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-20 13:29:31 +0200

    TopRated and MostOwned Fragments.

--- app/src/main/java/com/frisinacho/gangame/rated/TopRatedFragment.kt
+++ app/src/main/java/com/frisinacho/gangame/rated/TopRatedFragment.kt
@@ -4,33 +4,42 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import com.frisinacho.commons.BR
+import com.frisinacho.gangame.BR
 import com.frisinacho.commons.BaseListFragment
 import com.frisinacho.commons.DataBindingRecyclerAdapter
 import com.frisinacho.gangame.R
 import com.frisinacho.gangame.TopGame
+import com.frisinacho.gangame.data.GangameDataSource
 
 class TopRatedFragment : BaseListFragment(){
     override fun getAdapter(): RecyclerView.Adapter<*> {
         return DataBindingRecyclerAdapter<TopGame>(BR.topGame, R.layout.item_top_game)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        (listAdapter as DataBindingRecyclerAdapter<TopGame>)
-                .items.addAll(getDummyTopGames())
-        listAdapter.notifyDataSetChanged()
+    override fun onResume() {
+        super.onResume()
+        showTopRated()
     }
 
-    fun getDummyTopGames(): ArrayList<TopGame> {
-        return arrayListOf(TopGame("Counter Strike",
-                13407338,
-                80,
-                "Valve",
-                9.99F,
-                1,
-                "http://cdn.akamai.steamstatic.com/steam/apps/10/capsule_184x69.jpg"))
+    private fun showTopRated() {
+        GangameDataSource
+                .getTopRated()
+                .subscribe({list ->
+                    replaceItems(list)
+                }, { error ->
+                    showError(error)
+                })
     }
 
+    private fun replaceItems(list: List<TopGame>) {
+        with(listAdapter as DataBindingRecyclerAdapter<TopGame>){
+            items.clear()
+            items.addAll(list)
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun showError(error: Throwable) {
+        error.printStackTrace()
+    }
 }
