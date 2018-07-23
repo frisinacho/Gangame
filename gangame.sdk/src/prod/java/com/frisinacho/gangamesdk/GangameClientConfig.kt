06f7a71860c1d645ed48269dd1f1b73980130dd5
ignacio.lasaosa <frisinacho@gmail.com>
2018-07-23 10:59:56 +0200

    Improved Interceptor.

--- gangame.sdk/src/prod/java/com/frisinacho/gangamesdk/GangameClientConfig.kt
+++ gangame.sdk/src/prod/java/com/frisinacho/gangamesdk/GangameClientConfig.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 class GangameClientConfig: GangameApiConfig {
     override fun setupConfig(builder: Retrofit.Builder) {
         val okHttpClient = OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor())
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .build()
 
         builder.client(okHttpClient)
